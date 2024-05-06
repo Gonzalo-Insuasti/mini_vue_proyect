47632949239cb3c035a2ac4fb6626eecafbdd6b4
Gonzalo-Insuasti <Gonzaloinsuasty21@gmail.com>
2024-05-06 11:13:47 -0500

    Segundo cambio

--- platzi.js
+++ platzi.js
@@ -1,22 +1,44 @@
 class Platzireactive {
+    //*dependencias
+    deps = new Map();
+
     constructor(options){
         this.origen = options.data(); // origen
+
+        const self = this;
 
         //destino
         this.$data = new Proxy(this.origen,{
             //*GET dentro de este, recibimos de que objeto estamos intentando obtener una propiedad
-            get(target, name ){
-                if (Reflect.has(target, name)){
-                    return Reflect.get(target, name);
+            get(target, name, attr ){
+                if (Reflect.has(target, name, attr)){
+                    self.track(target, name, attr);
+                    return Reflect.get(target, name, attr);
                 }
                 console.warn('La propiedad', name, 'no exixte');
                 return '';
             },
             set(target, name, value){
-                console.log("modificando", target, value)
                 Reflect.set(target, name, value);
+                self.trigger(name);
             }
         });
+    }
+
+    track (target,name){
+        if (!this.deps.has(name)){
+            const effect = () => {
+                document.querySelectorAll(`*[p-text=${name}]`).forEach(el => {
+                    this.pText(el, target, name);
+                })
+            };
+            this.deps.set(name, effect); //aqui insertamos el efecto que creamos a las dependencias
+        }
+    }
+
+    trigger (name){
+        const effect = this.deps.get(name);
+        effect();
     }
     
     mount(){
@@ -30,6 +52,10 @@ class Platzireactive {
             el.addEventListener("input", () =>{
                 Reflect.set(this.$data, name, el.value);
             })
+        });
+        document.querySelectorAll("*[p-bind]").forEach(el =>{
+            const [attr, name] = el.getAttribute("p-bind").match(/(\w+)/g);
+            this.pBind(el, this.$data, name, attr);
         })
     }
     pText (el, target, name){
@@ -37,6 +63,10 @@ class Platzireactive {
     }
     pModel (el, target, name){
         el.value = Reflect.get(target,name);
+    }
+    pBind(el, target, attr) {
+        const [attribute, key] = attr.split(':');
+        el.setAttribute(attribute, Reflect.get(target, key));
     }
 }
 
